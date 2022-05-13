4e1aba890886ca732521d7754bfbc31754849bd2
Lorenzo Pavez <lorenzo.pavezfino@gmail.com>
2022-05-13 15:02:16 +0200

    Add new parameters for modifying Access-Control-Allow-Origin in order to bypass CORS issues (tested with Chrome 101)

--- bin/index.js
+++ bin/index.js
@@ -6,9 +6,14 @@ const NoCorsProxy = require('../lib')
 const port = argv.p || argv.port
 const host = argv.h || argv.host
 const target = argv.t || argv.target
+const callerHost = argv.callerHost || 'http://localhost'
+const callerPort = argv.callerPort || 4200
+const callerUrl = callerHost + ':' + callerPort
 
-const proxy = new NoCorsProxy(port, host, target)
+const proxy = new NoCorsProxy(port, host, target, callerUrl)
 proxy.start()
 
 console.info(`Proxying http://${proxy.config.host}:${proxy.config.port} to\
  => ${proxy.config.target}`)
+
+console.info('Access-Control-Allow-Origin is set to ' + proxy.config.origin)
